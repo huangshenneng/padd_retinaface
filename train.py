6a73cd0268f5ee2fa42018045fef33b96cbc8bd6
huangshenneng <15915816235@163.com>
2021-10-13 21:13:52 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,17 +1,15 @@
 #coding: utf-8
 from __future__ import print_function
 import os
-# import torch
-# import torch.optim as optim
+
 import paddle.optimizer as optim
-# import torch.backends.cudnn as cudnn
 import argparse
-# import torch.utils.data as data
 from paddle.io import Dataset, BatchSampler, DataLoader
 
 from data import WiderFaceDetection, detection_collate, preproc, cfg_mnet, cfg_re50
 from layers.modules import MultiBoxLoss
 from layers.functions.prior_box import PriorBox
+
 import time
 import datetime
 import numpy as np
@@ -22,8 +20,8 @@ import paddle
 
 parser = argparse.ArgumentParser(description='Retinaface Training')
 parser.add_argument('--training_dataset', default='./data/widerface/train/label.txt', help='Training dataset directory')
-# parser.add_argument('--network', default='mobile0.25', help='Backbone network mobile0.25 or resnet50')
-parser.add_argument('--network', default='resnet50', help='Backbone network mobile0.25 or resnet50')
+parser.add_argument('--network', default='mobile0.25', help='Backbone network mobile0.25 or resnet50')
+# parser.add_argument('--network', default='resnet50', help='Backbone network mobile0.25 or resnet50')
 parser.add_argument('--num_workers', default=0, type=int, help='Number of workers used in dataloading')
 parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, help='initial learning rate')
 parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
@@ -43,8 +41,11 @@ if args.network == "mobile0.25":
 elif args.network == "resnet50":
     cfg = cfg_re50
 
-cfg['batch_size']=3
-rgb_mean = (104, 117, 123) # bgr order
+cfg['batch_size']=2
+
+
+rgb_mean = (104, 117,  123) # bgr order
+
 num_classes = 2
 img_dim = cfg['image_size']
 num_gpu = cfg['ngpu']
@@ -61,24 +62,22 @@ training_dataset = args.training_dataset
 save_folder = args.save_folder
 
 net = RetinaFace(cfg=cfg)
-paddle.save(net.state_dict(),'model.pdparams')
-bb=paddle.load('model.pdparams')
-net.load_dict(paddle.load('model.pdparams'))
+
 print("Printing net...")
-# print(net)
+
 
 paddle.device.set_device("gpu")
 
 
-# cudnn.benchmark = True
+
 
 
 optimizer = optim.SGD(parameters=net.parameters(), learning_rate=initial_lr,  weight_decay=weight_decay)
 criterion = MultiBoxLoss(num_classes, 0.35, True, 0, True, 7, 0.35, False)
 
 priorbox = PriorBox(cfg, image_size=(img_dim, img_dim))
-# with torch.no_grad():
-priors = priorbox.forward()
+
+priors = priorbox.forward().detach()
 priors = priors.cuda()
 
 def train():
@@ -87,70 +86,79 @@ def train():
     print('Loading Dataset...')
 
     dataset_train = WiderFaceDetection( training_dataset,preproc(img_dim, rgb_mean))
-
     epoch_size = math.ceil(len(dataset_train) / batch_size)
     max_iter = max_epoch * epoch_size
 
-    stepvalues = (cfg['decay1'] * epoch_size, cfg['decay2'] * epoch_size)
-    step_index = 0
 
-    if args.resume_epoch > 0:
-        start_iter = args.resume_epoch * epoch_size
-    else:
-        start_iter = 0
-    # trainData=DataLoader(dataset_train, batch_size=2, shuffle=False, num_workers=0, collate_fn=detection_collate)
-    # trainData=DataLoader(dataset_train, batch_size=2, shuffle=True, num_workers=0, collate_fn=detection_collate)
-    trainData=DataLoader(dataset_train, batch_size=2, shuffle=True, num_workers=0)
+
+    train_loader=paddle.io.DataLoader(dataset_train, batch_size=batch_size, shuffle=True,num_workers=2,drop_last=True)
+    log_train = open('train.txt', 'a+')
+    print('------------------------- train : batchsize %d  ---------------------------' % batch_size)
+    print('------------------------- train : batchsize %d  ---------------------------' % batch_size, file=log_train)
+    show_time=10
+    accumulation_steps = 12
     for epoch in range(1000):
         print('epoch  ',epoch)
-        # for iteration, (images, targets) in enumerate(trainData()):
-        for iteration, (images) in enumerate(trainData()):
-            if iteration>1:
-                break
-            print('iteration  ',iteration)
+
+        epoch_image_index=[]
+        for iteration, (images,target) in enumerate(train_loader()):
 
             load_t0 = time.time()
-
-            # 把img和box进行分离
-            image_pd=images[:,:3,:,:]
-            target=images[:,3,:,:]
             targets=[]
+
             for tg in target:
-                num=(tg[0][0]).astype('int32')[0]
-                lbs=tg[1:num+1,0:15]
-                lbs=paddle.to_tensor(lbs,dtype='float32')
-                lbs.cuda()
+                tg=paddle.reshape(tg,[1,-1])[0]
+                num=tg[0].numpy().astype(np.int)[0]
+                lbs=paddle.ones([num,15],dtype='float64')
+                for i in range(num):
+                    lbs[i]=tg[i*15+1:(i+1)*15+1]
+                lbs=lbs.astype('float32')
                 targets.append(lbs)
 
-            images = paddle.to_tensor(image_pd, dtype='float32')
-            images = images.cuda()
-            images=images/128.
-
-            # forward
+            # print('images  shape',images.shape)
+            # #forward
             out = net(images)
 
-            # backprop
 
             loss_l, loss_c, loss_landm = criterion(out, priors, targets)
+
             loss = cfg['loc_weight'] * loss_l + loss_c + loss_landm
+            loss /= accumulation_steps
             loss.backward()
-            optimizer.step()
-            optimizer.clear_grad()
+            if ((iteration + 1) % accumulation_steps) == 0:
+                optimizer.step()
+                optimizer.clear_grad()
+
+
             load_t1 = time.time()
             batch_time = load_t1 - load_t0
             eta = int(batch_time * (max_iter - iteration))
-            print('Epoch:{}/{} || Epochiter: {}/{} || Iter: {}/{} || Loc: {:.4f} Cla: {:.4f} Landm: {:.4f} || LR: {:.8f} || Batchtime: {:.4f} s || ETA: {}'
-                  .format(epoch, max_epoch, (iteration % epoch_size) + 1,
-                  epoch_size, iteration + 1, max_iter, loss_l.item(), loss_c.item(), loss_landm.item(), initial_lr, batch_time, str(datetime.timedelta(seconds=eta))))
+            if iteration%show_time==0:
+                print('Epoch:{}/{} || Epochiter: {}/{} || Iter: {}/{} || Loc: {:.4f} Cla: {:.4f} Landm: {:.4f} || LR: {:.8f} || Batchtime: {:.4f} s || ETA: {}'
+                      .format(epoch, max_epoch, (iteration % epoch_size) + 1,
+                      epoch_size, iteration + 1, max_iter, loss_l.item(), loss_c.item(), loss_landm.item(), initial_lr, batch_time, str(datetime.timedelta(seconds=eta))))
+        #
+                print(
+                    'Epoch:{}/{} || Epochiter: {}/{} || Iter: {}/{} || Loc: {:.4f} Cla: {:.4f} Landm: {:.4f} || LR: {:.8f} || Batchtime: {:.4f} s || ETA: {}'
+                    .format(epoch, max_epoch, (iteration % epoch_size) + 1,
+                            epoch_size, iteration + 1, max_iter, loss_l.item(), loss_c.item(), loss_landm.item(), initial_lr,
+                            batch_time, str(datetime.timedelta(seconds=eta))) ,file=log_train)
+                log_train.close()
+                log_train = open('train.txt', 'a+')
 
-        # net.save(save_folder + cfg['name'] + '_Final',False)
-        paddle.save(net.state_dict(), save_folder + 'Final_Retinaface.pdparams')
+            del images
+            for lbs in targets:
+                del lbs
+            del targets
+
+        paddle.save(net.state_dict(), save_folder + '%d_Retinaface.pdparams'%epoch)
+
+    paddle.save(net.state_dict(), save_folder + 'Final_Retinaface.pdparams')
 
 
 def adjust_learning_rate(optimizer, gamma, epoch, step_index, iteration, epoch_size):
     """Sets the learning rate
-    # Adapted from PyTorch Imagenet example:
-    # https://github.com/pytorch/examples/blob/master/imagenet/main.py
+
     """
     warmup_epoch = -1
     if epoch <= warmup_epoch:
